39f534f92f673dd659828f4ac7f9476494950f53
Junior Alves <jjalves96@gmail.com>
2021-07-21 18:51:05 -0300

    feat: create constants for columns

---
+++ src/constants/colunm.tsx
@@ -0,0 +1,17 @@
+export const ColumnsFilms = [
+  {
+    name: 'Title',
+    selector: 'title',
+    sortable: true
+  },
+  {
+    name: 'Director',
+    selector: 'director',
+    sortable: true
+  },
+  {
+    name: 'Year',
+    selector: 'year',
+    sortable: true
+  }
+];
